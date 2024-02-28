9313196055b71030c44f34b891bb281fd0bf312f
mjay <michael@merged.group>
2024-02-28 16:53:29 -0500

    added note

--- src/common/decorators/bypass-verified-email-requirement.decorator.ts
+++ src/common/decorators/bypass-verified-email-requirement.decorator.ts
@@ -1,5 +1,12 @@
 import { SetMetadata } from '@nestjs/common';
 
+/**
+ * **** README ****
+ * This decorator should be used VERY judiciously
+ * At time of writing, I can't think of any reason to use it other than
+ * during user creation
+ */
+
 export const bypassVerifiedEmailRequirementMetadataName =
   'bypassVerifiedEmailRequirement';
 export const BypassVerifiedEmailRequirement = () =>
