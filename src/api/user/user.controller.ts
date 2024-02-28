165632221f9c206e8fa811710eecde9116ab18c0
mjay <michael@merged.group>
2024-02-28 15:38:56 -0500

    wrote and tested controller method createUser

--- src/api/user/user.controller.ts
+++ src/api/user/user.controller.ts
@@ -7,6 +7,7 @@ import {
 } from '../interfaces/authenticated-request.interface';
 import { BypassUserRequirement } from '../../common/decorators/bypass-user-requirement.decorator';
 import { UserService } from '../../internal-modules/user/user.service';
+import { SUCCESS_CODE } from '../../common/codes/success-codes';
 
 @Controller('user')
 export class UserController implements IUserController {
@@ -18,7 +19,11 @@ export class UserController implements IUserController {
   async createUser(req: AuthenticatedRequestForNewUser): Promise<any> {
     const { user } = req;
     const { name, email, external_auth_uid: externalAuthUID } = user;
-    return this.userService.createUser(name, email, externalAuthUID);
+    await this.userService.createUser(name, email, externalAuthUID);
+    return {
+      message: '', // This should succeed silently (from user perspective)
+      code: SUCCESS_CODE.UserCreated,
+    };
   }
 
   @BypassAccountRequirement()
