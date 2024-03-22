47fda06866b218789aa8edc454d0a3463696a76f
mjay <michael@merged.group>
2024-03-22 16:28:52 -0400

    Updated data access service spec

--- src/internal-modules/external-handlers/data-access/data-access.service.spec.ts
+++ src/internal-modules/external-handlers/data-access/data-access.service.spec.ts
@@ -2,14 +2,14 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { DataAccessService } from './data-access.service';
 
 describe('DataAccessService', () => {
-  let service: DataAccessService;
+  let service: DataAccessService<T>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [DataAccessService],
     }).compile();
 
-    service = module.get<DataAccessService>(DataAccessService);
+    service = module.get<DataAccessService<any>>(DataAccessService);
   });
 
   it('should be defined', () => {
--- src/system/modules/cache/cache.module.ts
+++ src/system/modules/cache/cache.module.ts
@@ -1,11 +1,10 @@
 import { Global, Module } from '@nestjs/common';
 import { CacheService } from './cache.service';
-import { LogModule } from '../log/log.module';
 import { CacheModule } from '@nestjs/cache-manager';
 
 @Global()
 @Module({
-  imports: [CacheModule.register(), LogModule],
+  imports: [CacheModule.register()],
   providers: [CacheService],
   exports: [CacheService],
 })
