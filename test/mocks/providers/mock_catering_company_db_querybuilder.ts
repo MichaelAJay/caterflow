2854702124288046db298e953b2194f5b2a7744f
mjay <michael@merged.group>
2024-04-03 17:18:57 -0400

    updated mock

--- test/mocks/providers/mock_catering_company_db_querybuilder.ts
+++ test/mocks/providers/mock_catering_company_db_querybuilder.ts
@@ -3,5 +3,5 @@ import { ICateringCompanyDbQueryBuilder } from 'src/internal-modules/external-ha
 export const mockCateringCompanyDbQueryBuilderService: ICateringCompanyDbQueryBuilder =
   {
     buildCreateCateringCompanyQuery: jest.fn(),
-    buildRetrieveCompanyIntegrationsListWhereClause: jest.fn(),
+    buildRetrieveCompanyIntegrationsListQueryWithoutInclude: jest.fn(),
   };
