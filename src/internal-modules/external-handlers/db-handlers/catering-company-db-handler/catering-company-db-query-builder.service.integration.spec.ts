aa13aee4a74673cce55761b772635491578c4d25
mjay <michael@merged.group>
2024-04-04 08:57:38 -0400

    wrote query builder integration tests

---
+++ src/internal-modules/external-handlers/db-handlers/catering-company-db-handler/catering-company-db-query-builder.service.integration.spec.ts
@@ -0,0 +1,199 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { CateringCompanyDbQueryBuilderService } from './catering-company-db-query-builder.service';
+import { IBuildRetrieveIntegrationListArgs } from './interfaces/query-builder-args.interfaces';
+import queryBuilderUtilities from './utilities/query-builder-utilities';
+
+describe('CateringCompanyDbQueryBuilderService', () => {
+  let service: CateringCompanyDbQueryBuilderService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [CateringCompanyDbQueryBuilderService],
+    }).compile();
+
+    service = module.get<CateringCompanyDbQueryBuilderService>(
+      CateringCompanyDbQueryBuilderService,
+    );
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  describe('buildRetrievecompanyIntegrationsListQueryWithoutInclude', () => {
+    const DEFAULT_PG_NUM = 1;
+    const DEFAULT_PER_PAGE = 10;
+
+    // passing
+    it('should return WHERE clause with companyId only when no query is provided', () => {
+      const companyId = 'abc123';
+      const query = undefined;
+      const result =
+        service.buildRetrieveCompanyIntegrationsListQueryWithoutInclude(
+          companyId,
+          query,
+        );
+
+      expect(query).toBeUndefined();
+      expect(result).toEqual({ where: { companyId }, take: DEFAULT_PER_PAGE });
+    });
+
+    // passing
+    it('should include isConfigured in WHERE clause when provided', () => {
+      const companyId = 'abc123';
+      const query: IBuildRetrieveIntegrationListArgs = { isConfigured: true };
+      const result =
+        service.buildRetrieveCompanyIntegrationsListQueryWithoutInclude(
+          companyId,
+          query,
+        );
+
+      expect(result).toEqual({
+        where: { companyId, isConfigured: true },
+        take: DEFAULT_PER_PAGE,
+      });
+    });
+
+    // passing
+    it('should include isActive in WHERE clause when provided', () => {
+      const companyId = 'abc123';
+      const query: IBuildRetrieveIntegrationListArgs = { isActive: false };
+      const result =
+        service.buildRetrieveCompanyIntegrationsListQueryWithoutInclude(
+          companyId,
+          query,
+        );
+
+      expect(result).toEqual({
+        where: { companyId, isActive: false },
+        take: DEFAULT_PER_PAGE,
+      });
+    });
+
+    // passing
+    it('should include createdSince in WHERE clause when provided', () => {
+      const companyId = 'abc123';
+      const createdSince = new Date('2023-01-01');
+      const query: IBuildRetrieveIntegrationListArgs = { createdSince };
+      const result =
+        service.buildRetrieveCompanyIntegrationsListQueryWithoutInclude(
+          companyId,
+          query,
+        );
+
+      expect(result).toEqual({
+        where: { companyId, createdAt: { gte: createdSince } },
+        take: DEFAULT_PER_PAGE,
+      });
+    });
+
+    // // TODO: Write test after refactor
+    // it('should include template srcSystem condition in WHERE clause when provided', () => {
+    //   const companyId = 'abc123';
+    //   const query: IBuildRetrieveIntegrationListArgs = {
+    //     templateSrcSystem: 'ezCater',
+    //   };
+    //   const result =
+    //     queryBuilderUtilities.buildRetrieveCompanyIntegrationsListWhereClause(
+    //       companyId,
+    //       query,
+    //     );
+
+    //   expect(result).toEqual({
+    //     companyId,
+    //     AND: [{ template: { srcSystem: 'ezCater' } }],
+    //   });
+    // });
+
+    // // TODO: Write test after refactor
+    // it('should include template targetSystem condition in WHERE clause when provided', () => {
+    //   const companyId = 'abc123';
+    //   const query: IBuildRetrieveIntegrationListArgs = {
+    //     templateTargetSystem: 'Nutshell',
+    //   };
+    //   const result =
+    //     queryBuilderUtilities.buildRetrieveCompanyIntegrationsListWhereClause(
+    //       companyId,
+    //       query,
+    //     );
+
+    //   expect(result).toEqual({
+    //     companyId,
+    //     AND: [{ template: { targetSystem: 'Nutshell' } }],
+    //   });
+    // });
+
+    // // TODO: Write test after refactor
+    // it('should include both template srcSystem and targetSystem conditions in WHERE clause when provided', () => {
+    //   const companyId = 'abc123';
+    //   const query: IBuildRetrieveIntegrationListArgs = {
+    //     templateSrcSystem: 'ezCater',
+    //     templateTargetSystem: 'Nutshell',
+    //   };
+    //   const result =
+    //     queryBuilderUtilities.buildRetrieveCompanyIntegrationsListWhereClause(
+    //       companyId,
+    //       query,
+    //     );
+
+    //   expect(result).toEqual({
+    //     companyId,
+    //     AND: [
+    //       { template: { srcSystem: 'ezCater' } },
+    //       { template: { targetSystem: 'Nutshell' } },
+    //     ],
+    //   });
+    // });
+
+    // // TODO: Write test after refactor
+    // it('should include all provided query parameters in WHERE clause', () => {
+    //   const companyId = 'abc123';
+    //   const createdSince = new Date('2023-01-01');
+    //   const query: IBuildRetrieveIntegrationListArgs = {
+    //     isConfigured: true,
+    //     isActive: true,
+    //     createdSince,
+    //     templateSrcSystem: 'ezCater',
+    //     templateTargetSystem: 'Nutshell',
+    //   };
+    //   const result =
+    //     queryBuilderUtilities.buildRetrieveCompanyIntegrationsListWhereClause(
+    //       companyId,
+    //       query,
+    //     );
+
+    //   expect(result).toEqual({
+    //     companyId,
+    //     isConfigured: true,
+    //     isActive: true,
+    //     createdAt: { gte: createdSince },
+    //     AND: [
+    //       { template: { srcSystem: 'ezCater' } },
+    //       { template: { targetSystem: 'Nutshell' } },
+    //     ],
+    //   });
+    // });
+
+    // passing
+    it('should include valid ORDERBY clause if query.sort is defined', () => {
+      const companyId = 'abc123';
+
+      const query: IBuildRetrieveIntegrationListArgs = { sort: 'created_asc' };
+      const result =
+        service.buildRetrieveCompanyIntegrationsListQueryWithoutInclude(
+          companyId,
+          query,
+        );
+
+      expect(result).toEqual({
+        where: { companyId },
+        take: DEFAULT_PER_PAGE,
+        orderBy: { createdAt: 'asc' },
+      });
+    });
+  });
+});
