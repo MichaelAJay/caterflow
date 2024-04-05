a359d65ddea9f4833196f5043a7d5c79e885929a
mjay <michael.a.jay@protonmail.com>
2024-04-04 22:58:37 -0400

    added middleware for get system integrations

---
+++ src/api/integration/middleware/get-integrations-validator-transformer/get-integrations-validator-transformer.middleware.ts
@@ -0,0 +1,34 @@
+import {
+  BadRequestException,
+  Injectable,
+  NestMiddleware,
+} from '@nestjs/common';
+import { FastifyReply, FastifyRequest } from 'fastify';
+import { IBuildRetrieveCompanyIntegrationListArgs } from '../../../../internal-modules/external-handlers/db-handlers/catering-company-db-handler/interfaces/query-builder-args.interfaces';
+import { validateGetSystemIntegrationsListQuery } from '../../validators/get.integration-lists';
+
+@Injectable()
+export class GetCompanyIntegrationsValidatorTransformerMiddleware
+  implements NestMiddleware
+{
+  use(req: FastifyRequest, res: FastifyReply, next: () => void) {
+    const query = req.query as unknown;
+
+    if (!validateGetSystemIntegrationsListQuery(query)) {
+      throw new BadRequestException({
+        error: 'Invalid query parameters',
+        details: validateGetSystemIntegrationsListQuery.errors,
+      });
+    }
+
+    const transformedQuery: IBuildRetrieveCompanyIntegrationListArgs = {
+      pg: query.pg ? parseInt(query.pg, 10) : undefined,
+      perPage: query.per_page ? parseInt(query.per_page, 10) : undefined,
+      templateSrcSystem: query.template_src,
+      templateTargetSystem: query.template_target,
+    };
+
+    req.query = transformedQuery;
+    next();
+  }
+}
