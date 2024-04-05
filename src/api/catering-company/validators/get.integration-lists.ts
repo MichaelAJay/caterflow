d7e7fd24f89f0b90d5008645239774c74ca0b452
mjay <michael.a.jay@protonmail.com>
2024-04-04 22:40:59 -0400

    renamed query type

--- src/api/catering-company/validators/get.integration-lists.ts
+++ src/api/catering-company/validators/get.integration-lists.ts
@@ -1,7 +1,7 @@
 import { JSONSchemaType } from 'ajv';
 import ajvSingleton from '../../../system/singletons/ajv.singleton';
 
-export type IGetIntegrationsQueryRequest = {
+export type IGetCompanyIntegrationsQueryRequest = {
   pg?: string; // must be parseable to int
   per_page?: string; // must be parseable to int
   filter_configured?: string; // 'true' | 'false'
@@ -12,36 +12,41 @@ export type IGetIntegrationsQueryRequest = {
   sort?: 'created_asc' | 'created_desc';
 };
 
-export const querySchema: JSONSchemaType<IGetIntegrationsQueryRequest> = {
-  type: 'object',
-  properties: {
-    pg: { type: 'string', pattern: '^\\d+$', nullable: true },
-    per_page: { type: 'string', pattern: '^\\d+$', nullable: true },
-    filter_configured: {
-      type: 'string',
-      enum: ['true', 'false'],
-      nullable: true,
+export const querySchema: JSONSchemaType<IGetCompanyIntegrationsQueryRequest> =
+  {
+    type: 'object',
+    properties: {
+      pg: { type: 'string', pattern: '^\\d+$', nullable: true },
+      per_page: { type: 'string', pattern: '^\\d+$', nullable: true },
+      filter_configured: {
+        type: 'string',
+        enum: ['true', 'false'],
+        nullable: true,
+      },
+      filter_active: {
+        type: 'string',
+        enum: ['true', 'false'],
+        nullable: true,
+      },
+      created_since: {
+        type: 'string',
+        enum: ['last_week', 'last_month', 'last_year'],
+        nullable: true,
+      },
+      template_src: { type: 'string', enum: ['ezCater'], nullable: true },
+      template_target: {
+        type: 'string',
+        enum: ['ezCater', 'Nutshell'],
+        nullable: true,
+      },
+      sort: {
+        type: 'string',
+        enum: ['created_asc', 'created_desc'],
+        nullable: true,
+      },
     },
-    filter_active: { type: 'string', enum: ['true', 'false'], nullable: true },
-    created_since: {
-      type: 'string',
-      enum: ['last_week', 'last_month', 'last_year'],
-      nullable: true,
-    },
-    template_src: { type: 'string', enum: ['ezCater'], nullable: true },
-    template_target: {
-      type: 'string',
-      enum: ['ezCater', 'Nutshell'],
-      nullable: true,
-    },
-    sort: {
-      type: 'string',
-      enum: ['created_asc', 'created_desc'],
-      nullable: true,
-    },
-  },
-  additionalProperties: false,
-};
+    additionalProperties: false,
+  };
 
 export const validateGetIntegrationsListQuery =
   ajvSingleton.compile(querySchema);
