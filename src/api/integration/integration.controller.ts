fdb66a73878c7d11e9b64fdb13f05e5f2e1a2c22
mjay <michael.a.jay@protonmail.com>
2024-04-04 22:34:37 -0400

    updated integration controller method & set metadata

--- src/api/integration/integration.controller.ts
+++ src/api/integration/integration.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, SetMetadata } from '@nestjs/common';
 import { IIntegrationController } from './interfaces/integration.controller.interface';
+import { $Enums } from '@prisma/client';
 
 @Controller('integration')
 export class IntegrationController implements IIntegrationController {
-  @Get()
+  @Get('list')
+  @SetMetadata('permissions', [$Enums.PermissionName.ManageIntegrations])
   async getIntegrations(): Promise<any> {
     throw new Error('Method not implemented.');
   }
